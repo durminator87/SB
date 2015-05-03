0c13dbbe744325ec5c1b4b594833774a649b114a
durminator87 <dula_93@hotmail.com>
2015-05-04 00:54:34 +0200

    Controls
    Enabled the crouch button

--- Assets/Standard Assets/2D/Scripts/Platformer2DUserControl.cs
+++ Assets/Standard Assets/2D/Scripts/Platformer2DUserControl.cs
@@ -4,37 +4,39 @@ using UnityStandardAssets.CrossPlatformInput;
 
 namespace UnityStandardAssets._2D
 {
-    [RequireComponent(typeof (PlatformerCharacter2D))]
-    public class Platformer2DUserControl : MonoBehaviour
-    {
-        private PlatformerCharacter2D m_Character;
-        private bool m_Jump;
-
-
-        private void Awake()
-        {
-            m_Character = GetComponent<PlatformerCharacter2D>();
-        }
-
-
-        private void Update()
-        {
-            if (!m_Jump)
-            {
-                // Read the jump input in Update so button presses aren't missed.
-                m_Jump = CrossPlatformInputManager.GetButtonDown("Jump");
-            }
-        }
-
-
-        private void FixedUpdate()
-        {
-			// Read the inputs.
-			bool crouch = Input.GetKey (KeyCode.LeftControl);
-            float h = CrossPlatformInputManager.GetAxis("Horizontal");
-            // Pass all parameters to the character control script.
-            m_Character.Move(h, crouch, m_Jump);
-            m_Jump = false;
-        }
-    }
+	[RequireComponent(typeof (PlatformerCharacter2D))]
+	public class Platformer2DUserControl : MonoBehaviour
+	{
+		private PlatformerCharacter2D m_Character;
+		private bool m_Jump;
+		
+		
+		private void Awake()
+		{
+			m_Character = GetComponent<PlatformerCharacter2D>();
+		}
+		
+		
+		private void Update()
+		{
+			if (!m_Jump)
+			{
+				// Read the jump input in Update so button presses aren't missed.
+				m_Jump = CrossPlatformInputManager.GetButtonDown("Jump");
+			}
+		}
+		
+		
+		private void FixedUpdate()
+		{
+			// Read the inputs.   
+			//Instead bool crouch = Input.GetKey (KeyCode.LeftControl); 
+			//I put the bellow code in order to crouch by pressing the corresponding button 
+			bool crouch = CrossPlatformInputManager.GetButton("Crouch");
+			float h = CrossPlatformInputManager.GetAxis("Horizontal");
+			// Pass all parameters to the character control script.
+			m_Character.Move(h, crouch, m_Jump);
+			m_Jump = false;
+		}
+	}
 }
