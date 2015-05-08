5869a527df5a2ccfa357758e965eebaed40b6cc1
durminator87 <dula_93@hotmail.com>
2015-05-08 23:38:48 +0200

    Created the menu for the game

---
+++ Assets/MainMenuScript.js
@@ -0,0 +1,23 @@
+﻿#pragma strict
+
+function Start () 
+{
+
+}
+
+function Update () 
+{
+
+}
+//this function loads the level1
+function StartGame () 
+{
+Application.LoadLevel("Level1");
+
+}
+//this function exits the game
+function ExitGame () 
+{
+Application.Quit();
+
+}
