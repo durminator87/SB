a72f960e70ea3e3fea11eb0a3938ac771fb39e27
durminator87 <dula_93@hotmail.com>
2015-05-13 23:56:10 +0200

    Improving

--- Assets/Scenes/Scripts/LoadNextLevel.cs
+++ Assets/Scenes/Scripts/LoadNextLevel.cs
@@ -2,14 +2,33 @@
 using System.Collections;
 
 public class LoadNextLevel : MonoBehaviour {
+	private bool PlayerInZone;
+	public string levelToLoad;
 
 	// Use this for initialization
 	void Start () {
-	
+		PlayerInZone = false;
 	}
 	
 	// Update is called once per frame
 	void Update () {
+		if (PlayerInZone) {
+			Application.LoadLevel(levelToLoad);
+		}
 	
+	}
+	void OnTriggerEnter2D(Collider2D other)
+	{
+		if (other.name == "Player") {
+			PlayerInZone=true;
+		}
+
+	}
+	void OnTriggerExit2D(Collider2D other)
+	{
+		if (other.name == "Player") {
+			PlayerInZone=false;
+		}
+		
 	}
 }
