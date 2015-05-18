d82dab5298826c812e6ea00bf897a80f167bf002
durminator87 <dula_93@hotmail.com>
2015-05-18 23:50:25 +0200

    Adding the elevator

--- Assets/Scenes/Scripts/MovingPlatform.cs
+++ Assets/Scenes/Scripts/MovingPlatform.cs
@@ -3,5 +3,30 @@ using System.Collections;
 
 public class MovingPlatform : MonoBehaviour {
 
+	public Transform DestinationSpot;
+	public Transform OriginSPot;
+	public float Speed;
+	public bool Switch = false;
 
+	void FixedUpdate()
+	{
+		if (transform.position == DestinationSpot.position) 
+		{
+			Switch = true;
+		}
+		if (transform.position == OriginSPot.position) 
+		{
+			Switch = false;
+		}
+		if(Switch)
+		{
+			transform.position = Vector3.MoveTowards(transform.position, OriginSPot.position, Speed);
+
+		}
+		else
+		{
+			transform.position = Vector3.MoveTowards(transform.position, DestinationSpot.position, Speed);
+			
+		}
+	}
 }
